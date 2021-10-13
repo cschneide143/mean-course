f910c06ea9261158f80fbc92fed815e2d13e391f
cschneider <cschneider143@gmail.com>
2021-10-12 22:06:44 -0500

    get posts from server

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Subject } from 'rxjs';
 
 import { Post } from './post.model';
@@ -8,8 +9,18 @@ export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
+  constructor(private http: HttpClient) {}
+
   getPosts() {
-    return [...this.posts];
+    //no need to unsubscribe, http package will handle the unsubscription.
+    this.http
+      .get<{ message: string; posts: Post[] }>(
+        'http://localhost:3000/api/posts'
+      )
+      .subscribe((postData) => {
+        this.posts = postData.posts;
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 
   getPostUpdateListener() {
@@ -17,7 +28,7 @@ export class PostsService {
   }
 
   addPost(ptitle: string, pcontent: string) {
-    const post: Post = { title: ptitle, content: pcontent };
+    const post: Post = { id: null, title: ptitle, content: pcontent };
     this.posts.push(post);
     this.postsUpdated.next([...this.posts]);
   }
