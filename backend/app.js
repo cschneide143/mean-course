d9e2027c1596f4c2b24ac83b24a615e2a94200a8
cschneider <cschneider143@gmail.com>
2021-10-12 21:42:22 -0500

    add api to fetch posts

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use( (req, res, next) => {
-  console.log('First middleware');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send('Hello from express!')
+app.use("/api/posts", (req, res, next) => {
+  const posts = [
+    {
+      id: "fakdjfkadjf",
+      title: "First server-side post",
+      content:
+        "Praesent laoreet diam eget est pellentesque, et pellentesque ex tristique. Cras et ullamcorper metus, a tempus nunc",
+    },
+    {
+      id: "jsadkjflkjlkjljkj",
+      title: "Second server-side post",
+      content:
+        "Praesent laoreet diam eget est pellentesque, et pellentesque ex tristique. Cras et ullamcorper metus, a tempus nunc",
+    },
+  ];
+  res.status(200).json({
+    message: "Posts fetched successfully!",
+    posts: posts,
+  });
 });
 
 module.exports = app;
