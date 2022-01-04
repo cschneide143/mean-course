05d5c70e46a66b6aa1a2e0efd7a150a1c60e1f70
cschneider <cschneider143@gmail.com>
2022-01-03 19:18:57 -0600

    Update post with image

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -50,12 +50,21 @@ router.post(
   }
 );
 
-router.put("/:id", (req, res, next) => {
+router.put("/:id", multer({ storage: storage }).single("image"), (req, res, next) => {
+  let imagePath = req.body.imagePath;
+  if (req.file) {
+    const url = req.protocol + '://' + req.get('host');
+    imagePath = url +"/images/" + req.file.filename
+  }
   const post = new Post({
     _id: req.body.id,
     title: req.body.title,
     content: req.body.content,
+    imagePath: imagePath
   });
+
+  console.log(post);
+
   Post.updateOne({ _id: req.params.id }, post).then((result) => {
     res.status(200).json({ message: "Update successful!" });
   });
