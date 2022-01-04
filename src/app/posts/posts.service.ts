dd47473deedb7a4f562fb0776e25812791971d29
cschneider <cschneider143@gmail.com>
2022-01-03 19:53:16 -0600

    Fixed bug on immutable field + uploaded new images

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -73,7 +73,7 @@ export class PostsService {
   updatePost(id: string, title: string, content: string, image: File | string) {
     let postData: Post | FormData;
     if (typeof(image) === 'object') {
-      const postData = new FormData();
+      postData = new FormData();
       postData.append("id", id);
       postData.append("title", title);
       postData.append("content", content);
